a3b877f6658f194f23619df4e44a36a545b75ef3
romerolucas1 <152455204+romerolucas1@users.noreply.github.com>
2024-06-19 10:42:51 -0300

    Create ACTIVIDAD2FACIL.PY

---
+++ ACTIVIDAD2FACIL.PY
@@ -0,0 +1,5 @@
+nombre_personaje = input("Ingrese el nombre de su personaje: ")
+
+num_pociones = int(input("Ingrese el número de pociones mágicas que posee: "))
+
+print(f"{nombre_personaje} es un valiente aventurero que posee {num_pociones} pociones mágicas.")
